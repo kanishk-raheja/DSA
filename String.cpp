9d0cb65046cd993aeb5ddb4b1bbcfd5d5be0cf30
Kanishk Raheja <147127794+kanishk-raheja@users.noreply.github.com>
2024-10-08 17:17:27 +0530

    String
    This C++ program illustrates how to create and initialize strings using the std::string class. Strings are sequences of characters, and in C++, the Standard Library provides the std::string class to work with them efficiently. This program covers different ways to create and initialize strings, showcasing how simple string manipulations can be done.
    
    In this program, we cover:
    
    Declaring a String using std::string.
    Initializing a String in various ways.
    Displaying the String using std::cout.

---
+++ String.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+
+    // Declare and initialize the string
+    string str1 = "Welcome to GeeksforGeeks!";
+
+    // Initialization by raw string
+    string str2("A Computer Science Portal");
+
+    // Print string
+    cout << str1 << endl << str2;
+
+    return 0;
+}

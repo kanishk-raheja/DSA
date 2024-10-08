8503fefb4afae2399503bd48b7ac24aba09a3a3d
Kanishk Raheja <147127794+kanishk-raheja@users.noreply.github.com>
2024-10-08 17:23:26 +0530

    Concatenation Of String
    This C++ program illustrates how to concatenate strings using the + operator. String concatenation is the process of combining two or more strings to form a single string. In C++, the std::string class supports the + operator, making it easy to join strings together.
    
    In this program, we cover:
    
    Basic String Concatenation using the + operator.
    Concatenating Multiple Strings in a single operation.
    Storing the Result of Concatenation in a new string variable.

---
+++ Concatenation.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+// Driver code
+int main()
+{
+	string init("this is init");
+	string add(" added now");
+
+	// Appending the string.
+	init = init + add;
+
+	cout << init << endl;
+	return 0;
+}

e0c3476cb3adb750e78270613b68799b10170393
Kanishk Raheja <147127794+kanishk-raheja@users.noreply.github.com>
2025-05-12 20:33:03 +0530

    1.ArrayCreation.cpp

---
+++ Array/1.ArrayCreation.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+int main()
+{
+    int n ;
+    cin>>n;
+    int arr[n];
+    cout<<"Input Array";
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    cout<<endl;
+    cout<<"Output Array";
+    for(int i=0;i<n;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+}

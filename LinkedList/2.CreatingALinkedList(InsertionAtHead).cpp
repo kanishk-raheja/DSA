aa3c7e2f89ca66c5c4d630c3948fdbb5f592874c
Kanishk Raheja <147127794+kanishk-raheja@users.noreply.github.com>
2025-05-26 12:31:57 +0530

    2.CreatingALinkedList(InsertionAtHead).cpp

---
+++ LinkedList/2.CreatingALinkedList(InsertionAtHead).cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+struct Node
+{
+    int data;
+    Node* next;
+    Node(int data)
+    {
+        this->data=data;
+        this->next=nullptr;
+    }
+};
+void InsertAtHead(Node*& head,int value)
+{
+    Node* newNode = new Node(value);
+    newNode -> next = head;
+    head = newNode;
+
+}
+void printList(Node* head) {
+    Node* temp = head;
+    while (temp != nullptr) {
+        cout << temp->data<<" "<<temp->next << " -> ";
+        temp = temp->next;
+    }
+    cout << "NULL" << endl;
+}
+int main()
+{
+    Node* head = nullptr;
+    int n;
+    cout<<"Enter No. Of Nodes:";
+    cin>>n;
+    int value;
+    for(int i=0;i<n;i++)
+    {
+        cout<<"Enter Value for Node "<<i+1<<":";
+        cin>>value;
+        InsertAtHead(head,value);
+    }
+    printList(head);
+}

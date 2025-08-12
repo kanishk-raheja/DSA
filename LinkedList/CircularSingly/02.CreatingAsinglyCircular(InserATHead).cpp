053e79f6366b97a41ae9f8b8e8f2eb8654cdf0d5
Kanishk Raheja <147127794+kanishk-raheja@users.noreply.github.com>
2025-08-12 11:50:51 +0530

    02.CreatingAsinglyCircular(InserATHead).cpp

---
+++ LinkedList/CircularSingly/02.CreatingAsinglyCircular(InserATHead).cpp
@@ -0,0 +1,62 @@
+#include<iostream>
+using namespace std;
+struct Node
+{
+    int data;
+    Node* next ;
+    Node(int data)
+    {
+        this->data=data;
+        this->next=nullptr;
+    }
+};
+void InsertAtHead(Node*& head,int data)
+{
+    Node* newNode = new Node(data);
+    if(head==nullptr)
+    {
+        head = newNode;
+        head->next = head;
+        return ;
+    }
+    Node* last = head;
+    while(last->next!=head)
+    {
+        last=last->next;
+    }
+    newNode->next=head;
+    last->next=newNode;
+    head=newNode;
+}
+
+void PrintList(Node* head)
+{
+    if(head==nullptr)
+    {
+        cout<<"List is empty"<<endl;
+        return;
+    }
+    Node* temp = head;
+    do
+    {
+        cout<<temp->data<<"->";
+        temp=temp->next;
+    } while (temp!=head);
+    cout<<"Head";
+    
+}
+int main()
+{
+    int n;
+    cout<<"No. of elements: ";
+    cin>>n;
+    Node* head = nullptr;
+    int data;
+    for(int i=0;i<n;i++)
+    {
+        cin>>data;
+        InsertAtHead(head,data);
+    }
+    cout<<"Original List: ";
+    PrintList(head);
+}

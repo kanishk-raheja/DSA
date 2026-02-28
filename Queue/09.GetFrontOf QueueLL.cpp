f21bfd9d4acdc964aa16f046ee3ca3da23b60ff6
Kanishk Raheja <147127794+kanishk-raheja@users.noreply.github.com>
2026-02-28 11:25:55 +0530

    Implement queue with get front functionality

---
+++ Queue/09.GetFrontOf QueueLL.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+using namespace std;
+
+struct Node{
+    int data;
+    Node* next;
+    Node(int data){
+        this->data = data;
+        this->next = nullptr;
+    }
+};
+
+struct Queue{
+    Node* front;
+    Node* rear;
+
+    Queue(){
+        front = nullptr;
+        rear = nullptr;
+    }
+
+    void enqueue(int element){
+        Node* newNode = new Node(element);
+
+        if(front == nullptr)
+            front = rear = newNode;
+        else{
+            rear->next = newNode;
+            rear = newNode;
+        }
+    }
+
+    int getfront(){
+        if(front == nullptr)
+            return -1;
+        return front->data;
+    }
+};
+
+int main(){
+    int n;
+    cin>>n;
+
+    Queue q;
+
+    for(int i=0;i<n;i++){
+        int element;
+        cin>>element;
+        q.enqueue(element);
+    }
+
+    cout<<q.getfront();
+}
